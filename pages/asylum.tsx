4b2c44ec372bad83820e3e4fd896c5770dc78459
Joey Scarim <joeyscarim@gmail.com>
2022-09-07 08:56:54 -0600

    update copy and images

--- pages/asylum.tsx
+++ pages/asylum.tsx
@@ -23,7 +23,9 @@ const WhatIsOperationStreamline: NextPage = () => {
 
       <section className="section-white">
         <div className="section-container flex flex-col md:flex-row gap-10">
-          <div className="w-full md:w-1/4">IMAGE</div>
+          <div className="w-full md:w-1/4">
+            <img src="/images/asylum-1.jpg" />
+          </div>
           <div className="w-full md:w-3/4">
             <h3>
               Too often, immigrants who are seeking asylum in the U.S. are
@@ -42,7 +44,7 @@ const WhatIsOperationStreamline: NextPage = () => {
             <h2>What Does the Law Say About Asylum Seekers?</h2>
           </div>
           <div className="w-full md:w-3/4">
-            <p> International Law</p>
+            <p className="underline"> International Law</p>
             <p>
               Following World War II, the United Nations created the Universal
               Declaration of Human Rights that recognized the right to seek
@@ -52,7 +54,7 @@ const WhatIsOperationStreamline: NextPage = () => {
               U.N. Protocol. The United States was a signatory to the 1967 U.N.
               Protocol and thus is bound by its provisions.
             </p>
-            <p>U.S. Law</p>
+            <p className="underline">U.S. Law</p>
             <p>
               The U.S. incorporated the U.N. asylum definitions into its own law
               in the Refugee Act of 1980. U.S. law says that any immigrant who
@@ -84,7 +86,9 @@ const WhatIsOperationStreamline: NextPage = () => {
             </ul>
           </div>
 
-          <div className="w-full md:w-1/4">IMAGE</div>
+          <div className="w-full md:w-1/4">
+            <img src="/images/asylum-2.jpg" />
+          </div>
         </div>
       </section>
 
@@ -118,74 +122,142 @@ const WhatIsOperationStreamline: NextPage = () => {
 
       <section className="section-white">
         <div className="section-container">
-          <h2>Reports Published by Other Organizations</h2>
+          {/* <div className="w-full md:w-3/4"> */}
+          <h2> How Is the U.S. Asylum Process Supposed to Work?</h2>
+          <p>
+            When immigrants enter the U.S. at a port of entry or in other ways,
+            they can tell a Border Patrol agent or other official that they are
+            seeking asylum in the U.S. The U.S. official should document this
+            and follow procedures to ensure that the immigrant’s asylum request
+            is evaluated fairly. The process is complex but includes these
+            steps:
+          </p>
+          <ol>
+            <li>
+              An initial screening known as a Credible Fear or Reasonable Fear
+              interview conducted by an immigration official. If individuals
+              fail this screening, they may be deported. They may also appeal
+              the ruling.
+            </li>
 
-          <ul>
             <li>
-              Indefensible: A Decade of Mass Incarceration of Migrants
-              Prosecuted for Crossing the Border.{' '}
-              <a
-                href="https://grassrootsleadership.org/reports/indefensible-decade-mass-incarceration-migrants-prosecuted-crossing-border"
-                target="_blank"
-                rel="noreferrer"
-              >
-                https://grassrootsleadership.org/reports/indefensible-decade-mass-incarceration-migrants-prosecuted-crossing-border
-              </a>
+              Immigrants who pass the initial screening may present their asylum
+              case before an immigration judge. There may be a long wait because
+              immigration courts have lengthy backlogs.
             </li>
+          </ol>
+
+          <p>
+            Asylum-seekers must be able to present convincing evidence that they
+            have a“well-foundedfear of persecution” in their home country due to
+            race, religion, nationality, political opinion, or membership in a
+            particular social group.
+          </p>
+          {/* </div> */}
+        </div>
+      </section>
+
+      <section className="section-gray">
+        <div className="container mx-auto flex gap-10">
+          <div className="w-1/3">
+            <h2>
+              What Really Happens to Immigrants Seeking Asylum in the U.S.
+            </h2>
+            <img src="/images/asylum-3.jpg" />
+          </div>
+          <div className="w-2/3">
+            <p>
+              Customs and Border Protection officials at ports of entry
+              sometimes turn away asylum seekers, falsely claiming that the U.S.
+              will not accept them. Border Patrol agents who apprehend border
+              crossers sometimes ignore or dismiss immigrants’ requests for
+              asylum and fail to document it. Agents may use the threat of
+              family separation as means to deter asylum seekers traveling with
+              children.(Link–
+              https://www.hopeborder.org/sealing-the-borderAnother link:
+              http://www.uscirf.gov/sites/default/files/Barriers%20To%20Protection.pdf
+            </p>
+            <p>
+              Individuals who attempt to enter the U.S. without authorization
+              are regarded as law-breakers and are prosecuted as criminals, even
+              if they have come to the U.S. to seek asylum. This violates
+              international prohibitions against punishing asylum-seekers.
+            </p>
+            <p>
+              During the prosecution process, the immigrant’s request for asylum
+              and right to a credible fear screening interview may be ignored or
+              lost in the maze of detention and prosecution.
+            </p>
+            <p>
+              The U.S. often incarcerates asylum seekers during the asylum
+              process, even those who presented at a port of entry and are not
+              being criminally prosecuted. This means that asylum seekers who
+              passed initial screening interviews and are waiting to present
+              their case to an immigration judge may be in an immigration
+              detention center for as long as two years or more.
+            </p>
+          </div>
+        </div>
+      </section>
+
+      <section className="section-white">
+        <div className="section-container">
+          {/* <div className="w-full md:w-3/4"> */}
+          <h2>
+            {' '}
+            What Happens to Asylum Seekers Who are Prosecuted in Streamline?
+          </h2>
+          <p>Border Patrol</p>
+          <p>
+            When Border Patrol agents apprehend migrants, they are supposed to
+            ask whether the border-crosser entered the U.S. to seek asylum, but
+            this happens inconsistently. Immigrant rights groups have documented
+            numerous occasions when Border Patrol agents ignore or disparage
+            migrants who say they are fleeing violence and oppression in their
+            home countries. Link:
+            http://www.uscirf.gov/sites/default/files/Barriers%20To%20Protection.pdf
+          </p>
+          <p>Defense Attorneys</p>
+          <p>
+            Apprehended migrants, including asylum-seekers, who are selected for
+            prosecution via Streamline will be bussed to the courthouse on their
+            court date, and will meet with an assigned defense attorney in the
+            morning for about 30 minutes. In this limited time, the attorney
+            should proactively ask defendants about their reasons for entering
+            the U.S., explain the process, and offer counsel. The attorney
+            should document the asylum request and take steps to ensure that the
+            defendant will get a credible fear interview.
+          </p>
+          <p>Rapid Deportation</p>
+          <p>
+            First-time border-crossers in Streamline usually get a sentence of
+            time served, and will be deported quickly back to their home
+            country. The migrant’s status as an asylum-seeker may be missed or
+            ignored, with no credible fear interview.
+          </p>
+
+          <ol>
             <li>
-              Streamline: Measuring its Effect on Illegal Border Crossing{' '}
-              <a
-                href="https://www.oig.dhs.gov/sites/default/files/assets/Mgmt/2015/OIG_15-95_May15.pdf"
-                target="_blank"
-                rel="noreferrer"
-              >
-                https://www.oig.dhs.gov/sites/default/files/assets/Mgmt/2015/OIG_15-95_May15.pdf
-              </a>
+              An initial screening known as a Credible Fear or Reasonable Fear
+              interview conducted by an immigration official. If individuals
+              fail this screening, they may be deported. They may also appeal
+              the ruling.
             </li>
+
             <li>
-              In Harm’s Way: Family Separation, Immigration Enforcement Programs
-              and Security on the U.S.-Mexico Border{' '}
-              <a
-                href="https://las.arizona.edu/sites/las.arizona.edu/files/SW%20Journal%20of%20Migration%20and%20Human%20Security%20Article%202015.pdf"
-                target="_blank"
-                rel="noreferrer"
-              >
-                https://las.arizona.edu/sites/las.arizona.edu/files/SW%20Journal%20of%20Migration%20and%20Human%20Security%20Article%202015.pdf
-              </a>
+              Immigrants who pass the initial screening may present their asylum
+              case before an immigration judge. There may be a long wait because
+              immigration courts have lengthy backlogs.
             </li>
-            <li>
-              Asylum in the United States{' '}
-              <a
-                href="https://americanimmigrationcouncil.org/research/asylum-united-states"
-                target="_blank"
-                rel="noreferrer"
-              >
-                https://americanimmigrationcouncil.org/research/asylum-united-states
-              </a>
-            </li>
-            <li>
-              Sealing the Border: The Criminalization of Asylum Seekers in the
-              Trump Era{' '}
-              <a
-                href="https://www.hopeborder.org/sealing-the-border"
-                target="_blank"
-                rel="noreferrer"
-              >
-                https://www.hopeborder.org/sealing-the-border
-              </a>
-            </li>
-            <li>
-              Barriers to Protection: The Treatment of Asylum Seekers in
-              Expedited Removal{' '}
-              <a
-                href="http://www.uscirf.gov/sites/default/files/Barriers%20To%20Protection.pdf"
-                target="_blank"
-                rel="noreferrer"
-              >
-                http://www.uscirf.gov/sites/default/files/Barriers%20To%20Protection.pdf
-              </a>
-            </li>
-          </ul>
+          </ol>
+
+          <p>
+            Asylum-seekers must be able to present convincing evidence that they
+            have a“well-foundedfear of persecution” in their home country due to
+            race, religion, nationality, political opinion, or membership in a
+            particular social group.
+          </p>
+          {/* </div> */}
         </div>
       </section>
 
--- pages/what-is-operation-streamline.tsx
+++ pages/what-is-operation-streamline.tsx
@@ -147,6 +147,183 @@ const WhatIsOperationStreamline: NextPage = () => {
         </div>
       </section>
 
+      <section className="section-white">
+        <div className="container mx-auto">
+          <h2>How Much Does This Cost the U.S Taxpayers?</h2>
+          <p>Total costs are unknown.</p>
+          <p>
+            Streamline courts have existed in multiple border communities since
+            2005, but after 14 years of this policy, no one knows what it costs.
+          </p>
+          <ul>
+            <li>
+              The Office of Inspector General for the Department of Homeland
+              Security faulted the DHS Office of Customs and Border Protection
+              (CBP) in 2015 for not tracking the full costs of Streamline.{' '}
+              <a href="https://www.oig.dhs.gov/sites/default/files/assets/Mgmt/2015/OIG_15-95_May15.pdf">
+                https://www.oig.dhs.gov/sites/default/files/assets/Mgmt/2015/OIG_15-95_May15.pdf
+              </a>
+            </li>
+            <li>
+              The U.S. Government Accountability Office also criticized CBP for
+              not accounting for all the costs of Streamline implementation,
+              focusing only on CBP’s own costs.{' '}
+              <a href="https://www.gao.gov/assets/690/682074.pdf">
+                https://www.gao.gov/assets/690/682074.pdf
+              </a>
+            </li>
+          </ul>
+          <p>Partial Estimates</p>
+          <ul>
+            <li>
+              In 2016, to mark ten years of Streamline, Grassroots Leadership
+              and Justice Strategies published a comprehensive report on the
+              history and impact of Streamline in all border areas. The report
+              estimated that the cost of incarceration over ten years totaled at
+              least $7 billion, but the authors did not attempt to calculate all
+              the other costs associated with Streamline prosecutions, such as
+              attorneys and other courtroom personnel.
+              <a href="https://grassrootsleadership.org/reports/indefensible-decade-mass-incarceration-migrants-prosecuted-crossing-border">
+                https://grassrootsleadership.org/reports/indefensible-decade-mass-incarceration-migrants-prosecuted-crossing-border
+              </a>
+            </li>
+            <li>
+              In 2019 the End Streamline Coalition researched the full costs of
+              conducting the daily Tucson, Arizona Streamline court, and
+              estimated a total of $62.4 million per year for Tucson alone.
+              <a href="https://endstreamline.org/wp-content/uploads/2020/02/Tucson-Operation-Streamline-Cost-Study-2-4-20.pdf">
+                https://endstreamline.org/wp-content/uploads/2020/02/Tucson-Operation-Streamline-Cost-Study-2-4-20.pdf
+              </a>
+            </li>
+          </ul>
+        </div>
+      </section>
+
+      <section className="section-gray">
+        <div className="container mx-auto flex gap-10">
+          <div className="w-1/3">
+            <h2>Are There Other Costs?</h2>
+            <img src="/images/what-3.jpg" />
+          </div>
+          <div className="w-2/3">
+            <p> Criminal conviction</p>
+            <p>
+              Acriminal conviction is a bar to future legal immigration for at
+              least 10 years; it also contributes to perceptions of immigrants
+              as“criminals.”
+            </p>
+            <p>Incarceration</p>
+            <p>
+              Incarceration results in lost income, separation from family,
+              trauma, and exposure to abuse while in immigration detention
+              centers.
+            </p>
+            <p>Clogged Courts</p>
+            <p>
+              U.S. federal courts are overwhelmed by immigrant prosecutions. In
+              2016 and 2017 immigration prosecutions made up almost half of all
+              federal prosecutions.
+            </p>
+            <p>Increased Migrant Deaths</p>
+            <p>
+              Migrants choose more remote locations to avoid apprehension and
+              are at greater risk of injury or death.
+            </p>
+            <p>Reduced circularity of migration</p>
+            <p>
+              Like all border enforcement efforts, Operation Streamline has
+              resulted in a decrease in the historically circular pattern of
+              Mexico-U.S. migration. As the risk of prosecution and imprisonment
+              increases, migrants are more likely to settle permanently in the
+              U.S. rather than risk returning periodically to their country of
+              origin.
+            </p>
+            <p>Enabling cartels</p>
+            <p>
+              U.S. strategies of border militarization and criminal prosecution
+              have spurred the growth of the human smuggling industry and
+              encouraged cartels to enter and control this business.
+            </p>
+          </div>
+        </div>
+      </section>
+
+      <section className="section-white">
+        <div className="container mx-auto">
+          <h2>Who Profits?</h2>
+          <p>Total costs are unknown.</p>
+          <p>
+            Many Streamlined immigrants serve time in private prisons run for
+            profit by well-connected corporations such as Core Civic and GEO
+            Group. Lobbyists for these corporations donate large sums of money
+            to federal election campaigns with the expectation of receiving
+            contracts for housing people criminalized by the federal government.
+          </p>
+          <ul>
+            <li>
+              According to ICE, in 2016 three quarters of immigration prisoners
+              were detained in private prisons.
+            </li>
+            <li>
+              According to the Migration Policy Institute, between 2007 and
+              2014, CoreCivic’s overall annual profits increased by 47% to $195
+              million. In the same time period, GEO Group’s annual profits grew
+              from about $42 million to $144 million – an increase of 243%.
+            </li>
+          </ul>
+        </div>
+      </section>
+
+      <section className="section-gray">
+        <div className="container mx-auto flex gap-10">
+          <div className="w-1/3">
+            <h2>Is Immigrant Criminal Prosecution an Effective Deterrent?</h2>
+            <img src="/images/what-4.jpg" />
+          </div>
+          <div className="w-2/3">
+            <p>
+              No large-scale studies have proved that criminal prosecution
+              deters unauthorized immigrants. Yet the Justice Department,
+              Department of Homeland Security, and many members of Congress
+              claim that Streamline“works.”The Office of Inspector General for
+              the Department of Homeland Security studied the Border Patrol
+              metrics for evaluating a deterrent effect and in 2015 found them
+              to be insufficient.{' '}
+              <a href="https://www.oig.dhs.gov/sites/default/files/assets/Mgmt/2015/OIG_15-95_May15.pdf">
+                https://www.oig.dhs.gov/sites/default/files/assets/Mgmt/2015/OIG_15-95_May15.pdf
+              </a>
+            </p>
+            <p>
+              Scholars agree that decisions to migrate are motivated by number
+              of factors, including economic need, threats of violence, natural
+              disasters, and family ties. Researchers from the U.S. and Mexico
+              interviewed a random sample of 1,100 recently-deported migrants in
+              six cities in Mexico between 2009 and 2012. Their analysis
+              differentiated respondents by whether they considered the U.S. to
+              be their permanent home, and by whether they intended to cross
+              into the U.S. again immediately(withina week) or at some time in
+              the future. Of those who considered the U.S. to be their home, 70%
+              intended to cross again at some point in the future.{' '}
+              <a href="https://las.arizona.edu/sites/las.arizona.edu/files/SW%20Journal%20of%20Migration%20and%20Human%20Security%20Article%202015.pdf">
+                https://las.arizona.edu/sites/las.arizona.edu/files/SW%20Journal%20of%20Migration%20and%20Human%20Security%20Article%202015.pdf
+              </a>
+            </p>
+            <p>
+              A 2018 report from the Vera Institute of Justice presented a time
+              series analysis of 22 years of immigrant apprehensions from
+              1992-2014 to assess how multiple factors might influence
+              fluctuations in border crossings over time. They used statistical
+              modeling to look at monthly data in each border sector and found
+              no evidence that the introduction of Streamline had a deterrent
+              effect in any of the border sectors.{' '}
+              <a href="https://www.vera.org/publications/operation-streamline">
+                https://www.vera.org/publications/operation-streamline
+              </a>
+            </p>
+          </div>
+        </div>
+      </section>
+
       <Footer />
     </div>
   );
